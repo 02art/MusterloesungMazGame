1905e0b4a33afba1459e6de13586a1a397b0df16
02art <148757401+02art@users.noreply.github.com>
2025-03-25 12:51:49 +0100

    Initial commit

---
+++ src/test/java/com/example/musterloesungmazgame/MusterloesungMazGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.musterloesungmazgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusterloesungMazGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
