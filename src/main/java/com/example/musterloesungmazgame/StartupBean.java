3b4e34c41a71e61c5e86c64d0bd869e3540123f8
02art <148757401+02art@users.noreply.github.com>
2025-04-29 19:22:45 +0200

    updated java to latest lts version (currently 21)

--- src/main/java/com/example/musterloesungmazgame/StartupBean.java
+++ src/main/java/com/example/musterloesungmazgame/StartupBean.java
@@ -4,7 +4,6 @@ import jakarta.annotation.PostConstruct;
 import org.openapitools.client.api.DefaultApi;
 import org.openapitools.client.model.*;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 @Component
 public class StartupBean {
@@ -14,7 +13,7 @@ public class StartupBean {
         DefaultApi defaultApi = new DefaultApi();
 
         GameInputDto gameInput = new GameInputDto();
-        gameInput.setGroupName("IrgendeinName"); //Group name
+        gameInput.setGroupName("TeamGewinner");
 
         GameDto response = defaultApi.gamePost(gameInput);
 
